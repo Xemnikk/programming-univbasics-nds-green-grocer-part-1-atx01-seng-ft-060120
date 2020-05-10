6774fbc0736c267caf516127ed802cb011163395
Toby Boudreaux <toby.boudreaux11@gmail.com>
2020-05-10 14:46:03 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -21,6 +21,9 @@ def consolidate_cart(cart)
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+  
+  new_array = []
+  
 
 end
 
